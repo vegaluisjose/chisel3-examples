07353ea28cdb586f36671283af30bfe500470b17
Luis Vega <vegaluisjose@gmail.com>
2019-08-01 21:34:08 -0700

    add different widths

--- cmac/src/main/scala/CMAC.scala
+++ cmac/src/main/scala/CMAC.scala
@@ -3,13 +3,14 @@ package example
 import chisel3._
 import chisel3.util._
 
-class CMAC(stages: Int = 8, length: Int = 8, dataBits: Int = 8) extends Module {
+class CMAC(stages: Int = 8, length: Int = 8, aBits: Int = 8, bBits: Int = 4) extends Module {
+  val mBits = aBits + bBits
   val io = IO(new Bundle {
-    val a = Flipped(ValidIO(Vec(length, SInt(dataBits.W))))
-    val b = Flipped(ValidIO(Vec(length, SInt(dataBits.W))))
+    val a = Flipped(ValidIO(Vec(length, SInt(aBits.W))))
+    val b = Flipped(ValidIO(Vec(length, SInt(bBits.W))))
     val y = ValidIO(SInt(32.W))
   })
-  val m = VecInit(Seq.fill(length)(0.asSInt((2*dataBits).W)))
+  val m = VecInit(Seq.fill(length)(0.asSInt((mBits).W)))
   for (i <- 0 until length) {
     m(i) := io.a.bits(i) * io.b.bits(i)
   }
